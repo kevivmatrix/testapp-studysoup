14fa42f12b10806974627650d83bf9b2f86be305
Keviv vivek <vivekpatna52@gmail.com>
2015-02-05 16:34:38 +0530

    small fix in contorller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    @products = Product.search(params[:query]) if params[:query].present?
+    @products = @products.search(params[:query]) if params[:query].present?
     @products = @products.filter(params[:filter]) if params[:filter].present?
     render json: @products
   end
